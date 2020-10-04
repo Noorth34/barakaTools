de4cda51b52e4842bf0c5428b32c49ec3f9a1d85
Gabriel Vidal <gabriel.vidal@gmx.fr>
2020-10-04 16:57:41 +0200

    add verification for QApplication that already exist. Create an instance if already exists.

--- main.py
+++ main.py
@@ -2,17 +2,21 @@
 
 """
 """
+import sys
 import mainDialog
 from PySide2.QtWidgets import QApplication
 
 
-reload(mainDialog)
+#reload(mainDialog)
 
-# OPTIMISATION: voir si le code du Dialog principal ne serait pas mieux directement dans le main.py
 
 if __name__ == "__main__":
-		
-	mainApp = QApplication.instance()
+
+	try:
+		mainApp = QApplication(sys.argv)
+	except:
+		mainApp = QApplication.instance()
+
 	mainWidget = mainDialog.MainDialogInstance()
 	mainWidget.show()
 	mainApp.exec_()
