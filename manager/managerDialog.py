b3d09acf6209c97588526d945522de12b32bc278
3D4 <3D4@ecmtp.local>
2020-10-12 13:08:48 +0200

    set up publisher basic ui elements

--- manager/managerDialog.py
+++ manager/managerDialog.py
@@ -12,11 +12,32 @@ class ManagerDialogInstance(QTabWidget):
 		self.setWindowTitle("Manager")
 		self.setWindowIcon(QIcon( BARAKA_ICONS_PATH + "/coca.png") )
 		self.setGeometry(600, 400, 225, 0)
-		self.setMinimumSize(225, 200)
+		self.setMinimumSize(225, 150)
 		self.setMaximumSize(600, 500)
 
 		self.initTabs()
-		
+		self.initPublisher()
+
+	def initPublisher(self):
+
+		self.layMain = QVBoxLayout(self.tabPublisher)
+		self.layPublisher = QGridLayout()
+
+		self.textfield = QLineEdit()
+		self.textfield.setPlaceholderText("Asset name...")
+		self.layMain.addWidget(self.textfield)
+
+		self.layMain.layout().addLayout(self.layPublisher)
+		editButton = QPushButton("Edit")
+		publishButton = QPushButton("Publish")
+		alembicCheckBox = QCheckBox("Is Alembic")
+
+		self.layPublisher.addWidget(editButton, 0, 1)
+		self.layPublisher.addWidget(publishButton, 0, 2)
+		self.layPublisher.addWidget(alembicCheckBox, 1, 2)
+
+
+
 
 	def initTabs(self):
 		
@@ -26,6 +47,9 @@ class ManagerDialogInstance(QTabWidget):
 		self.addTab(self.tabManager, "Manager")
 		self.addTab(self.tabPublisher, "Publisher")
 
+
+	def setSize(self, width, heigt):
+		self.setGeometry(0, 0, width, height)
 
 
 	def open(self):
