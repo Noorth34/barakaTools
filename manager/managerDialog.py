ef33bad83262af4a9bad7a27edb3aa6eff1d40dd
3D4 <3D4@ecmtp.local>
2020-10-12 11:25:38 +0200

    add tabs for manager dialog

--- manager/managerDialog.py
+++ manager/managerDialog.py
@@ -4,20 +4,29 @@ from PySide2.QtWidgets import *
 from PySide2.QtGui import QIcon
 from constants import *
 
-class ManagerDialogInstance(QWidget):
+class ManagerDialogInstance(QTabWidget):
 
 	def __init__(self):
 
-		QWidget.__init__(self)
+		QTabWidget.__init__(self)
 		self.setWindowTitle("Manager")
 		self.setWindowIcon(QIcon( BARAKA_ICONS_PATH + "/coca.png") )
 		self.setGeometry(600, 400, 225, 0)
 		self.setMinimumSize(225, 200)
 		self.setMaximumSize(600, 500)
 
+		self.initTabs()
+		
 
-	def initManagerDialog(self):
-		pass
+	def initTabs(self):
+		
+		self.tabManager = QWidget()
+		self.tabPublisher = QWidget()
+
+		self.addTab(self.tabManager, "Manager")
+		self.addTab(self.tabPublisher, "Publisher")
+
+
 
 	def open(self):
 		self.show()
