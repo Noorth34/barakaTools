e3a22cae1abc4677f85592d5088cd635862b55a7
3D4 <3D4@ecmtp.local>
2020-10-14 11:08:08 +0200

    add classes and methods for File, Path, Scene and Asset management

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,11 +2,11 @@
 
 import json
 import os
-import pymel.core as pm
+import pymel.core.system as pms
 
 class File():
 	"""
-	Functions for files management
+	Class for file management
 	"""
 	def __init__(self, path):
 		self.path = path
@@ -23,16 +23,103 @@ class File():
 		os.system( "attrib -h {}".format(backSlashPath) )
 
 
+class Scene():
+	"""
+	Class for maya scene management
+	"""
+	def __init__(self):
+
+		self.scene = pms.sceneName()
+
+
+	def getLongSceneName(self):
+
+		return self.scene
+
+	def getShortSceneName(self):
+
+		return self.scene.split("/")[-1]
+
+	def getNoExtensionShortSceneName(self):
+
+		return self.scene.getShortSceneName().split(".")[0]
+
+	def saveScene(self, type):
+
+		return pms.saveFile(type= type)
+
+	def saveSceneAs(self, dest):
+
+		return pms.saveAs(dest)
+
+	def exportSelection(self, dest, type):
+
+		return pms.exportSelected(dest, type= type)
+
+
+class Asset():
+	"""
+	"""
+	def __init__(self):
+		
+		self.name = "asset_state_type_index"
+
+	def initElements(self):
+
+		self.elements = self.name.split("_")
+		self.asset = self.elements[0]
+		self.state = self.elements[1]
+		self.type = self.elements[2]
+		self.index = self.elements[-1]
+
+	def setAsset(self, name):
+
+		self.initElements()
+		self.name = self.name.replace(self.asset, name)
+		return self.name
+
+	def setState(self, state):
+
+		self.initElements()
+		self.name = self.name.replace(self.state, state)
+		return self.name
+
+	def setType(self, type):
+
+		self.initElements()
+		self.name = self.name.replace(self.type, type)
+		return self.name
+
+	def setIndex(self, index):
+		
+		self.initElements()
+		self.name = self.name.replace(self.index, index)
+		return self.name
 
 
 class Path():
 	"""
 	Functions for path management
 	"""
-	def stringConvertSlash(src, dest):
+	def __init__(self, path):
 
+		self.path = path
+
+	def convertSlashToBackslash(self):
+
+		return self.path.replace("/", "\\")
+
+	def convertBackslashToSlash(self):
+
+		return self.path.replace("\\", "/")
+
+	def setIndex(self):
 		"""
 		"""
+
+
+
+
 
 
 class Commit():
