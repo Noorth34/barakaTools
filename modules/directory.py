9be592eef78f44c2072d9dde0e77791a672cd70a
3D4 <3D4@ecmtp.local>
2020-10-21 10:47:11 +0200

    copy file modules functions to directory module and wrap them to work with directory

--- modules/directory.py
+++ modules/directory.py
@@ -5,6 +5,15 @@ import shutil
 import modules.path as Path
 
 
+def isDir(func):
+	def inside(path, *args):
+		if Path.isDir(path) is False:
+			raise TypeError("This path doesn't refer to a directory")
+			return
+		func(path, *args)
+	return inside
+
+
 def createDir(path, name="_New_Dir"):
 
 	path = path + "/" + name
@@ -15,3 +24,53 @@ def createDir(path, name="_New_Dir"):
 def getChildren(path):
 	
 	return os.listdir(path)
+
+
+@isDir
+def copyTo(src=None, dest=None):
+
+	return shutil.copy(src, dest)
+
+@isDir
+def moveTo(src=None, dest=None):
+
+	return shutil.move(src, dest)
+
+
+@isDir
+def delete(path):
+
+	return shutil.rmtree(path)
+
+@isDir
+def getShortDirName(path):
+
+	return path.split("/")[-1]
+
+@isDir
+def getParent(path):
+
+	return os.path.dirname(path)
+
+@isDir
+def getRecursiveParent(path, iteration=1):
+
+	temp = None
+	for i in range(iteration):
+		temp = getParent(path)
+		path = temp
+
+	parent = path
+	return parent
+
+@isDir
+def setHidden(path):
+
+	backSlashPath = Path.convertSlashToBackslash(path)
+	os.system( "attrib +h {}".format(backSlashPath) )
+
+@isDir
+def setVisible(path):
+
+	backSlashPath = Path.convertSlashToBackslash(path)
+	os.system( "attrib -h {}".format(backSlashPath) )
