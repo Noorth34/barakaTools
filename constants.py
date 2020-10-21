44dd0646ff6a98c6047d73c0857f56cecc2585d3
3D4 <3D4@ecmtp.local>
2020-10-21 11:03:10 +0200

    add ressources path to constants

--- constants.py
+++ constants.py
@@ -4,12 +4,12 @@
 PIPELINE_ROOT_PATH = "//gandalf/3D4_20_21/barakafrites/04_asset"
 
 BARAKA_PATH = __file__.split("\\")[0]
-
 BARAKA_ICONS_PATH = BARAKA_PATH + "/icons"
-
 BARAKA_STYLESHEETS_PATH = BARAKA_PATH + "/qt/stylesheets"
 BARAKA_IMAGES_PATH = BARAKA_PATH + "/qt/images"
+BARAKA_RESSOURCES_PATH = BARAKA_PATH + "/ressources"
 
+ASSET_TEMPLATE = BARAKA_RESSOURCES_PATH + "/_template_workspace_asset"
 ASSET_TYPES = {
 	"Modeling" : "mod", 
 	"Rigging" : "rig", 
@@ -23,5 +23,5 @@ PIPELINE_CHARACTERS = PIPELINE_ROOT_PATH + "/character"
 PIPELINE_FX = PIPELINE_ROOT_PATH + "/FX" 
 PIPELINE_PROPS = PIPELINE_ROOT_PATH + "/prop"
 PIPELINE_SETS = PIPELINE_ROOT_PATH + "/set"	
-ASSET_TEMPLATE = PIPELINE_ROOT_PATH + "/_template_workspace_asset"
+
 
