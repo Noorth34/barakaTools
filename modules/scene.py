0bc708ee44b37fc6cd27c007ad758178ad46ca22
3D4 <3D4@ecmtp.local>
2020-10-22 17:57:50 +0200

    auto pep8

--- modules/scene.py
+++ modules/scene.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 
 import pymel.core.system as pms
 import modules.path as Path
@@ -14,64 +14,70 @@ _index = "index"
 
 def save(type):
 
-	return pms.saveFile(type= type)
+    return pms.saveFile(type=type)
 
 
 def saveAs(dest):
 
-	return pms.saveAs(dest)
+    return pms.saveAs(dest)
 
 
 def exportSelection(dest, type):
 
-	return pms.exportSelected(dest, type= type)
+    return pms.exportSelected(dest, type=type)
 
 
 def edit():
-	pass
+    pass
 
 
 def publish():
-	pass
+    pass
 
 
 def incrementIndex(scene):
 
-	if not scene:
-		scene = pms.sceneName()
-	scene = Path.deleteExtension(scene)
+    if not scene:
+        scene = pms.sceneName()
+    scene = Path.deleteExtension(scene)
 
-	currentIndex = int(scene.split("_")[-1])
-	newIndex = str(currentIndex + 1)
-	newIndex.zfill(4)
+    currentIndex = int(scene.split("_")[-1])
+    newIndex = str(currentIndex + 1)
+    newIndex.zfill(4)
 
-	scene = scene.replace(str(currentIndex), newIndex)
-	scene = Path.addExtension(scene, ".ma")
-	return scene
+    scene = scene.replace(str(currentIndex), newIndex)
+    scene = Path.addExtension(scene, ".ma")
+    return scene
 
 
 def createCharacter(name):
 
-	char = PIPELINE_CHARACTERS + "/{}".format(name)
-	Dir.copyTo(TEMPLATE_ASSET_DIRS, char)
+    char = PIPELINE_CHARACTERS + "/{}".format(name)
+    Dir.copyTo(TEMPLATE_ASSET_DIRS, char)
 
-	initScene = char + "/maya/scenes/edit/geo/{}_E_geo_0001.ma".format(name)
-	File.copyTo(TEMPLATE_ASSET_SCENE, initScene)
+    initScene = char + "/maya/scenes/edit/geo/{}_E_geo_0001.ma".format(name)
+    File.copyTo(TEMPLATE_ASSET_SCENE, initScene)
+
+    return char
 
 
 def createSet(name):
 
-	set = PIPELINE_SETS + "/{}".format(name)
-	Dir.copyTo(TEMPLATE_ASSET_DIRS, set)
+    set = PIPELINE_SETS + "/{}".format(name)
+    Dir.copyTo(TEMPLATE_ASSET_DIRS, set)
 
-	initScene = set + "/maya/scenes/edit/geo/{}_E_geo_0001.ma".format(name)
-	File.copyTo(TEMPLATE_ASSET_SCENE, initScene)
+    initScene = set + "/maya/scenes/edit/geo/{}_E_geo_0001.ma".format(name)
+    File.copyTo(TEMPLATE_ASSET_SCENE, initScene)
+
+    return set
 
 
 def createProp(name):
 
-	prop = PIPELINE_PROPS + "/{}".format(name)
-	Dir.copyTo(TEMPLATE_ASSET_DIRS, prop)
+    prop = PIPELINE_PROPS + "/{}".format(name)
+    Dir.copyTo(TEMPLATE_ASSET_DIRS, prop)
 
-	initScene = prop + "/maya/scenes/edit/geo/{}_E_geo_0001.ma".format(name)
-	File.copyTo(TEMPLATE_ASSET_SCENE, initScene)
+    initScene = prop + "/maya/scenes/edit/geo/{}_E_geo_0001.ma".format(name)
+    File.copyTo(TEMPLATE_ASSET_SCENE, initScene)
+
+    return prop
